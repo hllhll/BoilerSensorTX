2c14d9bb934bccfa381e1dd5a31b546eefa55d75
Hillel <h@h.com>
2022-10-15 15:32:59 +0300

    Almost revision 0.1 :)

--- BoilerSensorTX/src/main.cpp
+++ BoilerSensorTX/src/main.cpp
@@ -8,9 +8,17 @@
 //#define RF_DONT_LEAVE_SLEEP // For testing locally only
 #define JUST_TEST_RF_ON_BOOT
 #define DEBUG_PRINTS
-#define AVR_SLEEP_TIME 2
+// Options 2, 4, 8 in seconds
+#define AVR_SLEEP_TIME 8
 
-#if AVR_SLEEP_TIME==2 || AVR_SLEEP_TIME==4
+// in millis
+#define SAMPLE_INTERVAL 45000
+
+#if SAMPLE_INTERVAL<AVR_SLEEP_TIME
+  #error "Sample interval should be gt avr sleep time"
+#endif
+
+#if AVR_SLEEP_TIME==2 || AVR_SLEEP_TIME==4 || AVR_SLEEP_TIME==8
   #define AVR_SLEEP_TIME_OK
 #endif
 
@@ -48,7 +56,7 @@
 #include <avr/interrupt.h>
 
 bool hc12_is_atmode = false;
-bool avr_sleeping = false;
+bool avr_sleeping = true; // Start with sending data
 
 #include "Sensor.hpp"
 
@@ -72,82 +80,6 @@ OneWire  oneWire(SENSOR_1WIRE_PIN);  // (a 4.7K resistor is necessary)
 DallasTemperature sensors(&oneWire);
 
 unsigned int tx_counter;
-
-/*
-char CRC8(const byte *data,int length) 
-{
-   byte crc = 0x00;
-   byte extract;
-   byte sum;
-   for(int i=0;i<length;i++)
-   {
-      extract = *data;
-      for (char tempI = 8; tempI; tempI--) 
-      {
-         sum = (crc ^ extract) & 0x01;
-         crc >>= 1;
-         if (sum)
-            crc ^= 0x8C;
-         extract >>= 1;
-      }
-      data++;
-   }
-   return crc;
-}*/
-
-/*
-uint8_t CRC8(uint8_t *data, size_t len)
-{
-    uint8_t crc = 0xff;
-    size_t i, j;
-    for (i = 0; i < len; i++) {
-        crc ^= data[i];
-        for (j = 0; j < 8; j++) {
-            if ((crc & 0x80) != 0)
-                crc = (uint8_t)((crc << 1) ^ 0x31);
-            else
-                crc <<= 1;
-        }
-    }
-    return crc;
-}
-*/
-
-/*
-const uint8_t crc8x_table[256] = {
-0x00,0x31,0x62,0x53,0xC4,0xF5,0xA6,0x97,0xB9,0x88,0xDB,0xEA,0x7D,0x4C,0x1F,0x2E,
-0x43,0x72,0x21,0x10,0x87,0xB6,0xE5,0xD4,0xFA,0xCB,0x98,0xA9,0x3E,0x0F,0x5C,0x6D,
-0x86,0xB7,0xE4,0xD5,0x42,0x73,0x20,0x11,0x3F,0x0E,0x5D,0x6C,0xFB,0xCA,0x99,0xA8,
-0xC5,0xF4,0xA7,0x96,0x01,0x30,0x63,0x52,0x7C,0x4D,0x1E,0x2F,0xB8,0x89,0xDA,0xEB,
-0x3D,0x0C,0x5F,0x6E,0xF9,0xC8,0x9B,0xAA,0x84,0xB5,0xE6,0xD7,0x40,0x71,0x22,0x13,
-0x7E,0x4F,0x1C,0x2D,0xBA,0x8B,0xD8,0xE9,0xC7,0xF6,0xA5,0x94,0x03,0x32,0x61,0x50,
-0xBB,0x8A,0xD9,0xE8,0x7F,0x4E,0x1D,0x2C,0x02,0x33,0x60,0x51,0xC6,0xF7,0xA4,0x95,
-0xF8,0xC9,0x9A,0xAB,0x3C,0x0D,0x5E,0x6F,0x41,0x70,0x23,0x12,0x85,0xB4,0xE7,0xD6,
-0x7A,0x4B,0x18,0x29,0xBE,0x8F,0xDC,0xED,0xC3,0xF2,0xA1,0x90,0x07,0x36,0x65,0x54,
-0x39,0x08,0x5B,0x6A,0xFD,0xCC,0x9F,0xAE,0x80,0xB1,0xE2,0xD3,0x44,0x75,0x26,0x17,
-0xFC,0xCD,0x9E,0xAF,0x38,0x09,0x5A,0x6B,0x45,0x74,0x27,0x16,0x81,0xB0,0xE3,0xD2,
-0xBF,0x8E,0xDD,0xEC,0x7B,0x4A,0x19,0x28,0x06,0x37,0x64,0x55,0xC2,0xF3,0xA0,0x91,
-0x47,0x76,0x25,0x14,0x83,0xB2,0xE1,0xD0,0xFE,0xCF,0x9C,0xAD,0x3A,0x0B,0x58,0x69,
-0x04,0x35,0x66,0x57,0xC0,0xF1,0xA2,0x93,0xBD,0x8C,0xDF,0xEE,0x79,0x48,0x1B,0x2A,
-0xC1,0xF0,0xA3,0x92,0x05,0x34,0x67,0x56,0x78,0x49,0x1A,0x2B,0xBC,0x8D,0xDE,0xEF,
-0x82,0xB3,0xE0,0xD1,0x46,0x77,0x24,0x15,0x3B,0x0A,0x59,0x68,0xFF,0xCE,0x9D,0xAC };
-
-uint8_t CRC8(uint8_t* data, size_t len) {
-    uint8_t crc = 0xFF; // init value
-    for (size_t i = 0; i < len; i++) {
-         crc = crc8x_table[data[i] ^ crc];
-     }
-   return crc;
-}*/
-
-// stupid "add"
-/*uint8_t CRC8(uint8_t* data, size_t len) {
-  byte sum = 0;
-      for (size_t i = 0; i < len; i++) {
-         sum+=data[i];
-     }
-  return sum;
-}*/
 
 // https://stackoverflow.com/questions/51731313/cross-platform-crc8-function-c-and-python-parity-check
 uint8_t CRC8( uint8_t *addr, uint8_t len) {
@@ -245,14 +177,22 @@ ISR(WDT_vect) {
 void myWatchdogEnable() {  // turn on watchdog timer; interrupt mode every 2.0s
   cli();
   MCUSR = 0;
-#if AVR_SLEEP_TIME==2
+  // 7    6    5    4    3  2     1    0
+  //WDIF WDIE WDP3 WDCE WDE WDP2 WDP1 WDP0
   WDTCSR |= B00011000;
-  WDTCSR =  B01000111;
+#if AVR_SLEEP_TIME==2
+  WDTCSR =  B01000111; // 2 Seconds
   // DO NOT USE  wdt_enable!! It bricks!
   //wdt_enable(WDTO_4S);
+#elif AVR_SLEEP_TIME==4
+  WDTCSR =  B01100000; // 4 Seconds
+#elif AVR_SLEEP_TIME==8
+  WDTCSR =  B01100001; // 8 Seconds
 #endif
   sei();
 }
+
+unsigned long sleeping_millis = 0;
 
 void avr_sleep(){
   
@@ -262,6 +202,7 @@ void avr_sleep(){
 #ifdef DEBUG_PRINTS
   Serial.println("Slp");
 #endif
+  sleeping_millis+=AVR_SLEEP_TIME*1000;
   sleep_mode();
   
 }
@@ -782,23 +723,38 @@ void test_hc12(){
 }
 #endif
 
+unsigned long last_sample_millis = 0;
+bool first_loop = true;
+
 void loop() {
 #ifdef ACTIVATE_TEST_MODULE
   test_hc12();
   return;
 #endif
+  unsigned long cur_millis = millis() + (sleeping_millis);
   //if(false){
-  if(!avr_sleeping){
+  //if(!avr_sleeping){
+#ifdef DEBUG_PRINTS    
+    Serial.print("cur_millis = ");
+    Serial.print(cur_millis);
+    Serial.print("last_sample_millis = ");
+    Serial.println(last_sample_millis);
+    Serial.flush();
+#endif  
+  if( (!first_loop) && (cur_millis-last_sample_millis)<SAMPLE_INTERVAL ) {
+
     // Goto sleep
-    hc12_sleep();
+    // hc12_sleep();
     //stop_at(); // Leave AT so SLEEP takes effect- NO! DO NOT LEAVE AT, Current consumption is few uA lower
     avr_sleep();
   }else{
+    first_loop = false;
     avr_sleeping = !avr_sleeping;
+    last_sample_millis = millis() + sleeping_millis;
 #ifdef DEBUG_PRINTS    
     Serial.print("ON/Data\n");
-#endif
     Serial.flush();
+#endif
     //Serial.println("ASDASDASDASDSADSASDASDASDASDASDSSDSaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaADAS");
     //exit HC12 sleep
     mesure_and_send();
